e8beec270393c45cf54ac74ec437e081be0d61da
VCOSTA-Dev <vcosta@assessoriavcosta.com.br>
2021-06-12 17:00:06 -0300

    115. Adding Static Site Generation (SSG) On The Home Page

---
+++ helpers/api-util.js
@@ -0,0 +1,20 @@
+export async function getAllEvents() {
+    const response = await fetch('https://nextjs-c0299-default-rtdb.firebaseio.com/events.json');
+    const object = await response.json();
+
+    const events = [];
+
+    for (const key in object) {
+        events.push({
+            id: key,
+            ...object[key]
+        })
+    }
+
+    return events;
+
+}
+export async function getFeaturedEvents() {
+    const allEvents = await getAllEvents();
+    return allEvents.filter((event) => event.isFeatured);
+  }
