510898925f088aad36519cb63d6b7f7d30cf00c8
VCOSTA-Dev <vcosta@assessoriavcosta.com.br>
2021-06-13 21:48:10 -0300

    116. Loading Data & Paths For Dynamic Pages

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,20 +1,26 @@
 export async function getAllEvents() {
-    const response = await fetch('https://nextjs-c0299-default-rtdb.firebaseio.com/events.json');
-    const object = await response.json();
+  const response = await fetch(
+    'https://nextjs-c0299-default-rtdb.firebaseio.com/events.json'
+  );
+  const object = await response.json();
 
-    const events = [];
+  const events = [];
 
-    for (const key in object) {
-        events.push({
-            id: key,
-            ...object[key]
-        })
-    }
+  for (const key in object) {
+    events.push({
+      id: key,
+      ...object[key],
+    });
+  }
 
-    return events;
-
+  return events;
 }
 export async function getFeaturedEvents() {
-    const allEvents = await getAllEvents();
-    return allEvents.filter((event) => event.isFeatured);
-  }
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
